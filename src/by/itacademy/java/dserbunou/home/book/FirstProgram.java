4ddee210d16d58c81a3fb69c9ac7370809df7f16
SDmitry90 <sdmitry90@gmail.com>
2019-02-03 16:21:02 +0300

    ООП по книге

---
+++ src/by/itacademy/java/dserbunou/home/book/FirstProgram.java
@@ -0,0 +1,7 @@
+package by.itacademy.java.dserbunou.home.book;
+public class FirstProgram {
+	public static void main(String[] args) {
+		SloganAction firstObject = new SloganAction();
+		firstObject.printSlogan();
+	}
+}
---
+++ src/by/itacademy/java/dserbunou/home/book/SloganAction.java
@@ -0,0 +1,6 @@
+package by.itacademy.java.dserbunou.home.book;
+class SloganAction {
+void printSlogan() {
+	System .out .println ("Information tehnology");
+}
+}
