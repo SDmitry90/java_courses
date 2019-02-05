43119eba588b32b25e959439530cc1865231fa2e
114-9\user <SDmitry90@Gmail.com>
2019-02-05 20:55:40 +0300

    Адреса объектов

---
+++ src/by/itacademy/java/dserbunou/classroom/lesson2/Test.java
@@ -0,0 +1,20 @@
+package by.itacademy.java.dserbunou.classroom.lesson2;
+
+public class Test {
+	private static class Car {
+		private int year;
+
+		public Car(int year) {
+			this.year = year;
+		}
+		public int getYear() {
+			return year;
+		}
+	}
+	public static void main(final String[] args) {
+		Car car = new Car(2010);
+		Car similarCar = new Car(2010);
+		System.out.println(car == similarCar);
+	}
+
+}
