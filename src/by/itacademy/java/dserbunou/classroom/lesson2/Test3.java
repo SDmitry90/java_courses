f475136169ea25a532c76463aa22c0d4660f40eb
114-9\user <SDmitry90@Gmail.com>
2019-02-05 21:28:32 +0300

    if_else

---
+++ src/by/itacademy/java/dserbunou/classroom/lesson2/Test3.java
@@ -0,0 +1,13 @@
+package by.itacademy.java.dserbunou.classroom.lesson2;
+
+public class Test3 {
+	public static void main(String[] args) {
+		int x = 0, a = 2, b = 5;
+		if (a < b) {
+			x = b - a;
+		} else {
+			x = a - b;
+		}
+		System.out.println(x);
+	}
+}
