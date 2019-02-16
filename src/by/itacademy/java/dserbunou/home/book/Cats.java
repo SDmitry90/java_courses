df672cbe40f82b731ddf1948b23e6636aa9d6a0b
SDmitry90 <sdmitry90@gmail.com>
2019-02-16 23:38:52 +0300

    no message

---
+++ src/by/itacademy/java/dserbunou/home/book/Cats.java
@@ -0,0 +1,33 @@
+package by.itacademy.java.dserbunou.home.book;
+
+public class Cats {
+
+	public static void main(String[] args) {
+
+		int aNums[] = { 2, 4, 6 };
+
+//	укороченный вариант записи
+		/*
+		 * for (int num : aNums) { String strToPrint = num; }
+		 */
+
+		/*
+		 * int[] mice = { 4, 8, 10, 12, 16 }; int result = 0; for (int i = 0; i < 5;
+		 * i++) { result = result + mice[i]; } result = result / 5;
+		 * System.out.println("Среднее арифметическое: " + result);
+		 */
+
+		/*
+		 * int[] cats = new int[6]; cats[5] = 7;
+		 * 
+		 * String result = String.valueOf(cats[5]); System.out.println(result);
+		 */
+
+	}
+	/*
+	 * public void onClick(View view) { int[] cats = new int[6]; String result =
+	 * String.valueOf(cats[3]);
+	 * 
+	 * System.out.println(result); }
+	 */
+}
--- src/by/itacademy/java/dserbunou/home/practic1/SecondTask.java
+++ src/by/itacademy/java/dserbunou/home/practic1/SecondTask.java
@@ -7,38 +7,50 @@ import java.util.Arrays;
 
 public class SecondTask {
 
-    public static void main(String[] args) throws NumberFormatException, IOException {
+	public static void main(String[] args) throws NumberFormatException, IOException {
 
-        final BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+		final BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
 
-        System.out.println("enter array size:");
+		System.out.println("Введите размер массива:");
 
-        final int arraySize = Integer.parseInt(bufferedReader.readLine());
+		final int arraySize = Integer.parseInt(bufferedReader.readLine());
+		final int[] array = new int[arraySize];
 
-        final int[] array = new int[arraySize];
+		System.out.printf("Получили такой массив: %s \n", Arrays.toString(array));
 
-        for (int i = 0; i < arraySize; i++) {
-            System.out.printf("enter %s array element:\n", i);
-            final int element = Integer.parseInt(bufferedReader.readLine());
-            array[i] = element;
-        }
+		// запись значений в массив
+		for (int i = 0; i < arraySize; i++) {
+			System.out.printf("Введите %s элемент массива:\n", i);
+			final int element = Integer.parseInt(bufferedReader.readLine());
+			array[i] = element;
+		}
+		System.out.printf("Массив с введенными значениями %s \n", Arrays.toString(array));
 
-        System.out.printf("entered array %s:\n", Arrays.toString(array));
+		// обратный вывод значений на экран - для своего понимания
+		/*
+		 * for (int i = 0; i < array.length; i++) { int x = array[i];
+		 * System.out.printf("%s \n", x); }
+		 */
+		int maxL = 0;
+		int minL = 0;
+		int maxNumber = 0;
+		int minNumber = 0;
 
-        int numberShort = 0;
-   //     int numberLong = 0;
+		for (int d : array) {
+			String noname = d + "";
+			int length = noname.length();
+			System.out.printf("%s \n", length);
 
-        for (int element : array) {
-            final String Str = element + "";
-            final int length = Str.length();
-            if (length > numberShort) {
-                numberShort = length;
-                numberShort = element;
-            }
-            System.out.printf("maxlength =%s of element=%s", numberShort, numberShort);
-
-        }
-
-    }
-
+			if (length > maxL) {
+				maxL = length;
+				maxNumber = d;
+			}
+			if (length < minL || minL == 0) {
+				minL = length;
+				minNumber = d;
+			}
+		}
+		System.out.printf("Максимальное число %s с длиной %s \nМинимальное число %s с длинной %s \n", maxNumber, maxL,
+				minNumber, minL);
+	}
 }
