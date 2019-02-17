205137ec0bef4e3ce32b2b20ab9109feb6720592
SDmitry90 <sdmitry90@gmail.com>
2019-02-17 16:12:13 +0300

    over practic1

--- src/by/itacademy/java/dserbunou/classroom/lesson3/ArrayClass.java
+++ src/by/itacademy/java/dserbunou/classroom/lesson3/ArrayClass.java
@@ -3,8 +3,8 @@ package by.itacademy.java.dserbunou.classroom.lesson3;
 public class ArrayClass {
 
 	public static void main(String[] args) {
-		int[] rooms = new int[] { 1, 2, 3 }; //одномерный массив
-		int twoDim[][][][] = new int[4][5][6][7];//многомерный массив
+		int[] rooms = new int[] { 1, 2, 3 }; // одномерный массив
+		int twoDim[][]= new int[4][5];// многомерный массив
 	}
 
 }
--- src/by/itacademy/java/dserbunou/home/practic1/SecondTask.java
+++ src/by/itacademy/java/dserbunou/home/practic1/SecondTask.java
@@ -5,6 +5,8 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 
+import com.sun.org.apache.xpath.internal.axes.ReverseAxesWalker;
+
 public class SecondTask {
 
 	public static void main(String[] args) throws NumberFormatException, IOException {
@@ -26,36 +28,108 @@ public class SecondTask {
 		}
 		System.out.printf("Массив с введенными значениями %s \n", Arrays.toString(array));
 
-		averageValue(arraySize, array);
+		/*
+		 * maxMinValue(array);
+		 * 
+		 * averageValue(arraySize, array); double averageValue = averageValue(arraySize,
+		 * array); System.out.printf("Средняя длина %s \n", averageValue);
+		 * 
+		 * minAverageMaxAverage(array, averageValue);
+		 * 
+		 * evenNumbers(array);
+		 * 
+		 * dividedByThreeFive(array);
+		 * 
+		 * primeNumber(array);
+		 * 
+		 * isPalindrome(array);
+		 */
 
-		// double averageValue = 0;
-		// int[] maxLenght = new int[size];
+	}
 
-	/*	int sizeArray = 0;
+	private static void isPalindrome(final int[] array) {
+		for (int d : array) {
+			String noname = d + "";
+			String reverse = new StringBuffer(noname).reverse().toString();
+			if (noname.equals(reverse)) {
+				System.out.printf("Полиндром: %s \n", d);
+			}
+		}
+	}
+
+	private static void primeNumber(final int[] array) {
+		int i = 1;
+		for (int d : array) {
+			if ((d == 2 || d == 3 || d == 5 || d == 7) || (d % 2 != 0 && d % 3 != 0 && d % 5 != 0 && d % 7 != 0)) {
+				System.out.printf("%s простое число: %s \n", i, d);
+				i++;
+			}
+		}
+	}
+
+	private static void dividedByThreeFive(final int[] array) {
+		int i = 1;
+		for (int d : array) {
+			if (d % 3 == 0 && d % 5 == 0) {
+				System.out.printf("%s число, которое делится на 3 и на 5: %s \n", i, d);
+				i++;
+			}
+		}
+	}
+
+	private static void evenNumbers(final int[] array) {
+		int i = 1;
+		for (int d : array) {
+			if (d % 2 == 0) {
+				System.out.printf("%s четное число %s \n", i, d);
+				i++;
+			}
+		}
+		System.out.println("Больше четных чисел нет");
+	}
+	/*
+	 * System.out.printf("Числа с длинной меньше средней %s, \nбольше средней - %s",
+	 * Arrays.toString(minLenght), Arrays.toString(maxLenght));
+	 */
+
+	private static void minAverageMaxAverage(final int[] array, double averageValue) {
+		// int sizeArrayMin = 1;
+		// int sizeArrayMax = 1;
+		// int[] minLenght = new int[sizeArrayMin];
+		// int[] maxLenght = new int[sizeArrayMax];
+		int i = 1, j = 1;
 		for (int d : array) {
 			String noname = d + "";
 			int length = noname.length();
-			if (length < (averageValue(arraySize, array))) {
-				sizeArray++;
-				int[] minLenght = new int[sizeArray];
-				for (int i = 0; i < array.length; i++) {
-					minLenght[i] = d;
-				}
-
+			if (length <= averageValue) {
+				System.out.printf("%s число с длиной меньше средней %s \n", i, d);
+				i++;
+			} else {
+				System.out.printf("%s число с длиной больше средней %s \n", j, d);
+				j++;
 			}
-		}*/
-		maxMinValue(array);
+
+			/*
+			 * for (int i = 0; i < sizeArrayMin; i++) { minLenght[i] = d; sizeArrayMin++; }
+			 * minLenght[i] = d; sizeArrayMin++; i++; // sizeArrayMin++; // int[] minLenght
+			 * = new int[sizeArrayMin]; for (int i = 0; i < array.length; i++) {
+			 * minLenght[i] = d; sizeArrayMin++; } }else { maxLenght[j] = d; sizeArrayMax++;
+			 * j++; } else { //sizeArrayMax++; // int[] maxLenght = new int[sizeArrayMax];
+			 * for (int i = 0; i < array.length; i++) { maxLenght[i] = d; sizeArrayMax++; }
+			 */
+		}
 	}
 
-	private static void averageValue(final int arraySize, final int[] array) {
+	private static Double averageValue(final int arraySize, final int[] array) {
 		int size = 0;
+		double averageValue = 0;
 		for (int d : array) {
 			String noname = d + "";
 			int length = noname.length();
 			size += length;
-			double averageValue = (double) size / arraySize;
-			System.out.printf("Средняя длина %s \n", averageValue);
+			averageValue = (double) size / arraySize;
 		}
+		return averageValue;
 	}
 
 	private static void maxMinValue(final int[] array) {
