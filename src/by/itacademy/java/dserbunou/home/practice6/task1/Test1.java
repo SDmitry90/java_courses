e012855146e76a9e147a9211f0309c010b09e4bd
SDmitry90 <sdmitry90@gmail.com>
2019-03-25 00:31:42 +0300

    no message

--- src/by/itacademy/java/dserbunou/home/practice6/task1/Test1.java
+++ src/by/itacademy/java/dserbunou/home/practice6/task1/Test1.java
@@ -1,10 +1,59 @@
 package by.itacademy.java.dserbunou.home.practice6.task1;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Test1 {
 
-    public static void main(String[] args) {
-        // TODO Auto-generated method stub
+	public static void main(String[] args) throws FileNotFoundException, IOException {
+		File dir = new File("C:\\Test1");
+		dir.mkdir();
 
-    }
+		List<String> txt = addText();
 
+		for (String word : txt) {
+
+			File dir2 = new File("C:\\Test1", word.substring(0, 1));
+			dir2.mkdir();
+
+			// boolean created = dir.mkdir();
+
+			try (FileWriter fw = new FileWriter("words.txt", true);
+					BufferedWriter bw = new BufferedWriter(fw);
+					PrintWriter out = new PrintWriter(bw)) {
+				out.println(word.substring(0, 1));
+				out.println(word);
+			} catch (IOException e) {
+			}
+
+		}
+
+	}
+
+	private static List<String> addText() throws IOException, FileNotFoundException {
+		List<String> txt = new ArrayList<String>();
+		String text = null;
+		try (FileReader reader = new FileReader("SampleTextFile_10kb (1).txt");
+				BufferedReader br = new BufferedReader(reader);) {
+			while ((text = br.readLine()) != null) {
+				text = text.replaceAll("[,.]", "");
+				putInSet(text.split(" "), txt);
+			}
+		}
+		return txt;
+	}
+
+	public static void putInSet(String[] arr, List<String> list) {
+		for (String word : arr) {
+			list.add(word);
+		}
+	}
 }
