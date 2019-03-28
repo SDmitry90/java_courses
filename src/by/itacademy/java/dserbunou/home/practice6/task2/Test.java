a18b1f4bc8c702eae7a33c317b299b395407d071
SDmitry90 <sdmitry90@gmail.com>
2019-03-28 23:41:08 +0300

    no message

--- src/by/itacademy/java/dserbunou/home/practice6/task2/Test.java
+++ src/by/itacademy/java/dserbunou/home/practice6/task2/Test.java
@@ -1,28 +1,64 @@
 package by.itacademy.java.dserbunou.home.practice6.task2;
 
 import java.io.File;
-import java.util.Arrays;
-import java.util.List;
-
-import javax.swing.filechooser.FileSystemView;
+import java.io.IOException;
 
 public class Test {
 
 	public static void main(String[] args) {
+		getLetterC();
 
-		getLetter();
+		printThree(new File("C:\\")); // не вышло в путь вставить полученную букву диска
+
+		printThree2(new File("C:\\"));
 
 	}
 
-	private static void getLetter() {
-		List<File> files = Arrays.asList(File.listRoots());
-		for (File f : files) {
-			String s1 = FileSystemView.getFileSystemView().getSystemDisplayName(f);
-			// String s2 = FileSystemView.getFileSystemView().getSystemTypeDescription(f);
-			System.out.println("getSystemDisplayName : " + s1);
-			// System.out.println("getSystemTypeDescription : " + s2);
-			break;
+	public static void printThree(File f) {
+
+		if (!f.isDirectory()) {
+			System.out.println(f.getName());
 		}
+
+		if (f.isDirectory()) {
+			try {
+				System.out.println(f.getCanonicalFile());
+				File[] child = f.listFiles();
+
+				for (int i = 0; i < child.length; i++) {
+					System.out.println(child[i].getParent());
+					printThree(child[i]);
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	private static void printThree2(File paths) {
+		File[] child = paths.listFiles();
+		if (child != null)
+			for (File dir : child) {
+				if (dir.isDirectory())
+					try {
+						System.out.println(dir.getCanonicalPath());
+						printThree2(dir);
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+			}
+	}
+
+	private static String getLetterC() {
+		File[] letter = File.listRoots();
+		/*
+		 * for (File file : letter) { System.out.println(file.getAbsolutePath()); break;
+		 * }
+		 */
+		System.out.println(letter[0].getAbsolutePath());
+		String c = letter[0].getAbsolutePath();
+		return c;
+
 	}
 
 }
