501c0be338b544839617603c9a012e7d71c9c1b2
114-9\user <SDmitry90@Gmail.com>
2019-04-26 21:27:34 +0300

    SQL запросы

--- src/by/itacademy/java/dserbunou/home/practice7/ui/Application.java
+++ src/by/itacademy/java/dserbunou/home/practice7/ui/Application.java
@@ -1,11 +1,13 @@
 package by.itacademy.java.dserbunou.home.practice7.ui;
 
+import java.io.IOException;
+
 import by.itacademy.java.dserbunou.home.practice7.ui.commands.AbstractCmd;
 import by.itacademy.java.dserbunou.home.practice7.ui.commands.CmdHome;
 
 public class Application {
 
-	public static void main(String[] args) throws InstantiationException, IllegalAccessException {
+	public static void main(String[] args) throws InstantiationException, IllegalAccessException, IOException {
 		System.out.println("старт программы");
 
 		AbstractCmd nextCommand = new CmdHome();
