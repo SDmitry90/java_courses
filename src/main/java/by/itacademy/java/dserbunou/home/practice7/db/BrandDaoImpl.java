ea995ed51ff1b9054921ae334e75019afbc9b4a0
114-9\user <SDmitry90@Gmail.com>
2019-05-10 20:26:14 +0300

    Convert to Maven

--- src/main/java/by/itacademy/java/dserbunou/home/practice7/db/BrandDaoImpl.java
+++ src/main/java/by/itacademy/java/dserbunou/home/practice7/db/BrandDaoImpl.java
@@ -1,7 +1,5 @@
 package by.itacademy.java.dserbunou.home.practice7.db;
 
-import by.itacademy.java.dserbunou.home.practice7.db.entity.Brand;;
-
 public abstract class BrandDaoImpl implements AbstractDao<Brand> {
 
 }
