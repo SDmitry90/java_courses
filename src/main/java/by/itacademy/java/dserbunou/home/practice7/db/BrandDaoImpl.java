81027e9c3353429875d815808b81df158a5a4a2f
114-9\user <SDmitry90@Gmail.com>
2019-05-10 21:23:38 +0300

    Maven_web

--- src/main/java/by/itacademy/java/dserbunou/home/practice7/db/BrandDaoImpl.java
+++ src/main/java/by/itacademy/java/dserbunou/home/practice7/db/BrandDaoImpl.java
@@ -1,5 +1,7 @@
 package by.itacademy.java.dserbunou.home.practice7.db;
 
+import by.itacademy.java.dserbunou.home.practice7.db.entity.Brand;
+
 public abstract class BrandDaoImpl implements AbstractDao<Brand> {
 
 }
